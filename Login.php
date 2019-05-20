a1e382a097ea1863b257e184c868651615fea248
lirimimeri <lirimimeri17@gmail.com>
2019-05-20 22:40:10 +0200

    Login...

---
+++ Login.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Login</title> 
+        <link rel="stylesheet" type="text/css" href="css/Login.css">
+    </head>
+    <body>
+        
+        <div class="login">
+            <a><img src="images/login.png" class="img"></a>
+            <p class="text">Perdoruesi</p>
+            <input type="text" class="input">
+            <p class="text" style="">Fjalekalimi</p>
+            <input type="password" class="input">
+
+            <button class="hyrja" type="submit">Hyrja</button>
+        </div>
+    
+    
+    
+
+    </body>
+</html>
