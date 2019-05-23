40ecdd3d3f77745e5a9c7bcb3918debbf98d417c
lirimimeri <lirimimeri17@gmail.com>
2019-05-23 15:42:51 +0200

    Update fetchStudentData.php

--- fetchStudentData.php
+++ fetchStudentData.php
@@ -1,133 +1,142 @@
 <?php
 
-
-if(session_status() == PHP_SESSION_NONE) {
+//	nese sesioni ska fillu
+if (session_status() == PHP_SESSION_NONE) {
+// fillo sesionin
     session_start();
 }
 
-//Parandalimi i qasjes direkte ne databaz pa u kyqur
-if(isset($_SESSION['credentialsEntered'])) {
+//parandalimi i qasjes direkte ne databaz pa u logirat
+if (isset($_SESSION['credentialsEntered'])) {
 ?>
-<html>
-    <head>
-        <link rel='stylesheet icon' type='text/css' href='https://fonts.googleapis.com/css?family=Nothing+You+Could+Do'>
-        <title>Fetch Student Data</title>
-        <!-- CSS/foto ... -->
 
+    <html>
+    <head>
+    <!-- Google fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Nothing+You+Could+Do' rel='stylesheet' type='text/css'>
+    <!-- CSS style sheets -->
+    <link rel="StyleSheet" href="./css/registration-design.css" />
+    <link rel="StyleSheet" href="./css/message-design.css" />
+    <title>Fetch Student Data</title>
+    <link rel="shortcut icon" type="image/png" href="./pictures/westmount.png"/>
     </head>
     <body>
-        <!-- Butonat per me shtu student ne databaz -->
-        <table cellspacing = "8">
-            <tr>
-                <td>
-                <form action="./addNewStudent.php" method="post">
-                    <input type="submit" name="go" class="redirectButton" value="Add Student to Database">
-                </td>
-            </tr>
-                </form> 
-        </table>
 
-        <?php
-        //lidhja me databaz
-        require_once('dbconfig.php');
-        
-        //fshirja pi databaze nese butoni esht kliku
-        if(isset($_POST['student_id'])) {
-            $student_id = $_POST['student_id'];
-            if(isset($_POST['delete'])){
-                $sql = "DELETE FROM students WHERE student_id = ". $student_id;
-                if(mysqli_query($dbc, $sql)){
-                    ?>
-                    <div class="isa_success">
-                        <i class="fa fa-check"></i>
-                        <?php echo "Student Deleted"; ?>
-                    </div>
-                    <?php
-                }
-                else {
-                    ?>
-                    <div class="isa_error">
-                        <i class="fa fa-warning"></i>
-                        <?php echo "Error Ocurred ".mysqli_error($link); ?>
-                    </div>
-                    <?php
-                }
-            }
+    <!-- Butoni per me shtu student ne databaz -->
+    <table align="center" cellspacing = "8">
+    <tr>
+        <td align>
+        <form action="./addStudent.php" method="post">
+            <input type="submit" name="go"  class="redirectButton" value="Add Student to Database" />
+        </td>
+    </tr>
+        </form>
+    </table>
+
+<?php
+// konektimi me databaz
+require_once('dbconfig.php');
+
+//	fshrija e te dhenes specifike nga databaza kur te klikohet butoni
+
+if (isset($_POST['student_id'])) {
+        $student_id = $_POST['student_id'];
+        if (isset($_POST['delete'])) {
+            $sql = "DELETE FROM students WHERE student_id = " . $student_id;
+			
+		    if (mysqli_query($conn, $sql))
+			{?>
+				<!-- Outputi pas fshirjes -->
+				<div class="isa_success">
+				<i class="fa fa-check"></i>
+					<?php echo "Student Deleted"; ?>
+				</div><?php
+			}
+			else
+				// Outputi nese ndodh naj gabim gjat fshirjes
+			{?>
+				<div class="isa_error">
+				<i class="fa fa-warning"></i>
+					<?php echo "Error Occurred ".mysqli_error($link); ?>
+				</div><?php
+			}
         }
+    }
+?>
+
+<div id = "title">
+    <h1>Student Information Database:</h1>
+    <img src="./pictures/header.png" alt="Banner" style="width:600px;height:100px;"/>
+</div>
+
+<?php
+
+//	Krijimi i queryt per databaz
+$query = "SELECT first_name, last_name, gender, email, street_name, city,
+phone,d_day, d_month, d_year, student_id FROM students ORDER BY last_name, first_name";
+
+// Marrja e pergjigjes nga databaza duke derguar lidhjen dhe queryn
+$response = @mysqli_query($conn, $query);
+
+// nese query ekzekutohet mir
+if($response){
+    //	Outputi i numrit te studenteve me te dhena
+    $num_rows = mysqli_num_rows($response);
+    echo "<p align='center'><font face = 'Architects Daughter' size='4pt'><i> * $num_rows student records fetched! </i></font></p>";
+
+
+    echo '<table class = "studentinfo" align="center" cellpadding="8">
+
+    <tr><td align="left"><font face = "Architects Daughter" size="3"><b>Last Name</b></font></td>
+    <td align="left"><font face = "Architects Daughter" size="3"><b>First Name</b></font></td>
+    <td align="left"><font face = "Architects Daughter" size="3"><b>Gender</b></font></td>
+    <td align="left"><font face = "Architects Daughter" size="3"><b>Email</b></font></td>
+    <td align="left"><font face = "Architects Daughter" size="3"><b>Street</b></font></td>
+    <td align="left"><font face = "Architects Daughter" size="3"><b>City</b></font></td>
+    <td align="left"><font face = "Architects Daughter" size="3"><b>Phone</b></font></td>
+    <td align="left"><font face = "Architects Daughter" size="3"><b>Birth Date</b></font></td>';
+
+    // Kthe nje rresht te te dhenave nga query derisa t'mos ket te dhena tjera
+    while($row = mysqli_fetch_array($response)){
+        echo '<tr><td align="left">' . 
+        $row['last_name'] . '</td><td align="left">' . 
+        $row['first_name'] . '</td><td align="left">' .
+        $row['gender'] . '</td><td align="left">' .
+        $row['email'] . '</td><td align="left">' . 
+        $row['street_name'] . '</td><td align="left">' .
+        $row['city'] . '</td><td align="left">' . 
+        $row['phone'] . '</td><td align="left">' .
+        $row['d_month'] ." ".$row['d_day'] .", ".$row['d_year'] .'</td><td align="left">';
         ?>
-        <div id="title">
-            <h1>Student Infromation Database:</h1>
-            <img src="" alt="Banner" style="width: 600px; height: 100px;" />
-        </div>
-        
-        <?php
-        //query
-        $query = "SELECT first_name, last_name, gender, email, street_type, city, postal_code,
-        phone, course, dob_day, dob_month, dob_year, student_id FROM students ORDER BY last_name, first_name";
 
-        //pergjigja nga databaza duke derguar connection dhe query
-        $response = @mysqli_query($dbc, $query);
-
-        if($response) {
-            $num_rows = mysqli_num_rows($response);
-            echo "<p align='center'><font face='Architects Daughter' size='4pt'><i> * $num_rows student records fetched! </i></font></p>";
-            
-            echo '<table class = "studentinfo" align="center" cellpadding="8">
-                <tr><td align="left"><font face = "Architects Daughter" size="3"><b>Last Name</b></font></td>
-                <td align="left"><font face = "Architects Daughter" size="3"><b>First Name</b></font></td>
-                <td align="left"><font face = "Architects Daughter" size="3"><b>Gender</b></font></td>
-                <td align="left"><font face = "Architects Daughter" size="3"><b>Email</b></font></td>
-                <td align="left"><font face = "Architects Daughter" size="3"><b>Street</b></font></td>
-                <td align="left"><font face = "Architects Daughter" size="3"><b>City</b></font></td>
-                <td align="left"><font face = "Architects Daughter" size="3"><b>Postal Code</b></font></td>
-                <td align="left"><font face = "Architects Daughter" size="3"><b>Phone</b></font></td>
-                <td align="left"><font face = "Architects Daughter" size="3"><b>Birth Date</b></font></td>
-                <td align="left"><font face = "Architects Daughter" size="3"><b>Course Code</b></font></td></tr>';
-
-                //kthimi i nje rreshti me te dhena nga query perderisa te gjitha fushat jane te plotesuara
-                while($row = mysqli_fetch_array($response)) {
-                    echo '<tr><td align="left">' .
-                    $row['last_name'] . '</td><td align="left">'.
-                    $row['first_name'] . '</td><td align="left">'.
-                    $row['gender'] . '</td><td align="left">' . 
-                    $row['email'] . '</td><td align="left">' . 
-                    $row['street_name'] ." ".$row['street_type'].'</td><td align="left">' .
-                    $row['city'] . '</td><td align="left">' . 
-                    $row['postal_code'] . '</td><td align="left">' . 
-                    $row['phone'] . '</td><td align="left">' .
-                    $row['dob_month'] ." ".$row['dob_day'] .", ".$row['dob_year'] .'</td><td align="left">'.
-                    $row['course'] . '</td><td align="left">';
-        ?>
-
-        <form action="" method="post">
-            <td><input type="hidden" id="student_id" name="student_id" value="<?php echo $row['student_id']; ?>" /></td>
+        <!-- eventat ne form qendrojn ne faqe -->
+        <form method="post" action="">
+            <!-- Butonai fshehur ruan id specifike te studentit -->
+            <td><input type="hidden" id = "student_id" name="student_id" value="<?php echo $row['student_id']; ?>" /></td>
+            <!-- butoni delete -->
             <td><input type="submit" name="delete" class="deleteButton" value="Delete" onclick="return confirm('Are you sure?')" /></td>
         </form>
 
         <?php
         echo '</tr>';
-                }
-                echo '</table>';
-            }
+    }		
+echo '</table>';
+} 
+//	nese query nuk ekzekutohet si duhet
+else {
+    echo "Couldn't issue database query<br />";
+    echo mysqli_error($conn);
+}
 
-            else {
-                echo "Couldn't issue database query<br />";
-                echo mysqli_error($dbc);
-            }
-
-            mysqli_close($dbc);
-        }
-        else{
-            require_once('login.php');
-        }
-        ?>
-
-
-
-
-                
-        
-
-    </body>
+// mbyllja e konektimit me databaz
+mysqli_close($conn);
+}
+//	nese var e sesionit nuk vendoset
+else
+{
+	//	useri/admini duhet mu logirat per me kqyr databazen
+	require_once('Login.php');
+}
+?>
+</body>
 </html>
-    
